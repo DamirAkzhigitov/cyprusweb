cb2bc68c780f815c75dc11054eda98c0638578ec
Damir Akzhigitov <damir.ckelet@gmail.com>
2022-02-18 00:55:29 +0300

    initial setup amplify

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,9 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports.js";
+
+Amplify.configure(awsExports);
 
 createApp(App).use(store).use(router).mount("#app");
